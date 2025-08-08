f704d0f812d5a976600ff6e2fe2007ef528b1fc5
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-08 19:21:32 +0300

    header footer body

---
+++ EduSync/src/components/Body.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='body'>
+        <div className='sudent' onClick={() => alert('Content Clicked!')}>
+            hi
+            </div>
+      Body
+    </div>
+  )
+}
+
+export default Body
