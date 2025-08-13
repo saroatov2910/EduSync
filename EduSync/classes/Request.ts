82636c86e42c2cc7f61df3122da3c5afef4e7534
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-13 13:37:08 +0300

    Add inheritance from Student

--- EduSync/classes/Request.ts
+++ EduSync/classes/Request.ts
@@ -1,33 +1,38 @@
-import React from 'react';
+// Request.ts
+import Student from './Student';
+// Utility functions
+export default class Request extends Student {
+  requestId: string;
+  requestText: string;
+  requestDate: string;
+  reqStatus: string;
 
-export default class Request {
-  requestId = '';
-  requestText = '';
-  requestDate = '';
-  reqStatus = '';
-
-  constructor(requestId, requestText, requestDate, reqStatus) {
+// Constructor to initialize Request object
+  constructor(
+    studentId: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    requestId: string,
+    requestText: string,
+    requestDate: string,
+    reqStatus: string
+  ) {
+    // Call the parent constructor
+    super(studentId, firstName, lastName, email); 
     this.requestId = requestId;
     this.requestText = requestText;
     this.requestDate = requestDate;
     this.reqStatus = reqStatus;
   }
+// Method to create a Request object from a plain object
+  printInfo(): void {
+    console.log(`Request ID: ${this.requestId}`);
+    console.log(`Student ID: ${this.StudentId}`);
+    console.log(`Name: ${this.firstName} ${this.lastName}`);
+    console.log(`Email: ${this.email}`);
+    console.log(`Request Text: ${this.requestText}`);
+    console.log(`Request Date: ${this.requestDate}`);
+    console.log(`Status: ${this.reqStatus}`);
+  }
 }
-
-// class Request extends React.Component{
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             requestId: props.requestId,
-//             requestText: props.requestText,
-//             requestDate: props.requestDate,
-//             reqSstus: props.reqStatus,
-//             studentId. props.studentId,
-
-//         };
-
-//     }
-
-
-// }
- 
