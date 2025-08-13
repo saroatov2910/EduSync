b4844e6e103bfe2aa9105122a82d2213e391f73c
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-13 13:10:53 +0300

    Creat Request class

--- EduSync/classes/ContactMsg.ts
+++ EduSync/classes/ContactMsg.ts
@@ -1,10 +1,15 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react'; 
 
+class Msg extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            msgId : props.msgId,
+            msgText : props.msgText,
+            msgDate : props.msgDate,
+            msgSender : props.msgSender,
+        }
 
+    }
 
-// class Msg extends Component{    
-
-
-    
-
-//   }
+}
