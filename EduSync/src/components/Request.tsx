c3ec20c9e00b26628b565e874df8a63f2b533046
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-15 16:56:09 +0300

    validateText - fiexed

--- EduSync/src/components/Request.tsx
+++ EduSync/src/components/Request.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import  Request from '../../classes/Request';
+import  Request from '../classes/Request';
 import '../cssRules/Body.css';
 
 const RequestTable: React.FC = () => {
