c0606660c8663fae9ba15b9b04229d98f92d33fa
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-16 13:59:29 +0300

    add the constructor

--- EduSync/src/classRequest/Request.ts
+++ EduSync/src/classRequest/Request.ts
@@ -1,25 +1,58 @@
 // Request.ts
 import Student from '../classStudent/Student';
-import { isValidDate, validateText } from '../Functions/dateUtils';
-import
+import type { RequestTopic, RequestStatus } from '../RequestStatus';
+import { isValidStudentId, validateText, isValidDate } from '../Functions/dateUtils';
+
+export interface RequestProps {
+  studentId: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobile: string;
+  major: string;
+  requestId: number;
+  requestTopic: RequestTopic;
+  requestText: string;
+  requestDate: Date;
+  reqStatus: RequestStatus;
+  
+
+  handlerId: number;
+}
 
 
 export default class Request extends Student {
   requestId: number;
+  requestTopic: RequestTopic;
   requestText: string;
-  requestDate: Date; 
-  reqStatus: string;
+  requestDate: Date;
+  reqStatus: RequestStatus;
+  handlerId: number; 
 
-  constructor(
-    studentId: number,
-    requestId: number,
-    requestText: string,
-    requestDate: Date, 
-    reqStatus: string
-  ) {
-    super(studentId, '', '', '', '', ''); 
+  constructor(props: RequestProps) {
+    super({
+      studentId: props.studentId,
+      firstName: props.firstName,
+      lastName: props.lastName,
+      email: props.email,
+      mobile: props.mobile,
+      major: props.major
+    });
 
-
+    this.requestId = props.requestId;
+    this.requestTopic = props.requestTopic;
+    this.requestText = props.requestText;
+    this.requestDate = props.requestDate;
+    this.reqStatus = props.reqStatus;
+    
+    this.handlerId = props.handlerId;
+    
+    this.validate();
   }
 
+  protected validate() {
+    isValidStudentId(this.studentId);
+    validateText(this.requestText);
+    isValidDate(this.requestDate);
+  }
 }
