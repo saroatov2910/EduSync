c87089947cbb1d5c42384418eef6ed0f97df6754
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-16 13:58:15 +0300

    add the constructor

--- EduSync/src/RequestStatus.ts
+++ EduSync/src/RequestStatus.ts
@@ -1,6 +1,7 @@
 export type RequestStatus = "פתוחה" | "סגורה" | "בטיפול";
 export type AppointmentType = "פתוחה" | "סגורה" | "בטיפול";
-export type createdBy= "סטודנט" | " ממרצה טפל"| "אחר";
+export type createdBy= "מזכירות|סטודנט" | " ממרצה טפל"| "אחר";
 export type grade = 1 | 2 | 3 | 4 | 5 ;
 export type RequestTopic = "נושא אקדמי" | "בעיה טכנית" | "בקשה מנהלית" | "אחר";
+
 
