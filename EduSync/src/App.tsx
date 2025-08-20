e451c33c493b46d7800444a3c432b40db6681b4c
gallib28 <gallibenson123@gmail.com>
2025-08-20 09:33:42 +0300

    fixing header file name

--- EduSync/src/App.tsx
+++ EduSync/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/layout/Header';
+import Header from './components/layout/HeaderRoot';
 import Footer from './components/layout/Footer';
 import StudentTable from "./components/Student";
 
 function App() {
   return (
     <div>
-      <Header />
+      <HeaderRoot />
       <main>
         <h1>טבלת סטודנטים</h1>
         <StudentTable />
