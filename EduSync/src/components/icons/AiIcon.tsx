9b05e2aa6c3b054373f1daee7cf89460354dd3c6
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-10-06 02:16:40 +0300

    Add all JSON files

---
+++ EduSync/src/components/icons/AiIcon.tsx
@@ -0,0 +1,10 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+export default function AiIcon(props: any) {
+  return (
+    <SvgIcon {...props}>
+      {/* כאן שמים path של האייקון שלכם */}
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 …" />
+    </SvgIcon>
+  );
+}
--- EduSync/src/global.d.ts
+++ EduSync/src/global.d.ts
@@ -1,1 +1,4 @@
 declare module '*.css';
+declare module '*.css';
+declare module '*.wasm' { const url: string; export default url; }
+declare module '*.wasm?url' { const url: string; export default url; }
